93980f2e813e7aedc45391c4eedd833ab7ff69ba
GEOHYEON <phoibos97@naver.com>
2022-03-10 11:09:16 +0900

    연립

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -1,23 +1,26 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [title, editTitle] = useState(['남자 코트 추천', '여자 코트 추천']);
+
+  function hello_world() {
+    return 'Hello, world.'
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          안녕하세요
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>blog</div>
+      </div>
+      <p>{hello_world()}</p>
+      <div className='list'>
+        <h4>{ title[0] }</h4>
+        <p>3월 10일</p>
+        <hr/>
+      </div>
     </div>
   );
 }
