89857f67071cc953d80a26a99edf2640c3875fe0
GEOHYEON <phoibos97@naver.com>
2022-06-07 13:58:30 +0900

    google cloiud

--- react/blog/src/App.js
+++ react/blog/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
 
-  let [title, editTitle] = useState(['남자 코트 추천', '여자 코트 추천']);
-
+  let [title, editTitle] = useState(['남자 코트 추천', '여자 코트 추천', '어린이 코트 추천']);
+  let [good, editGood] = useState(0);
   function hello_world() {
     return 'Hello, world.'
   }
@@ -17,8 +17,18 @@ function App() {
       </div>
       <p>{hello_world()}</p>
       <div className='list'>
-        <h4>{ title[0] }</h4>
+        <h4>{ title[0] } <span onClick={()=>{editGood(good+1)}}>good</span> {good} </h4>
         <p>3월 10일</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h4>{ title[1] }</h4>
+        <p>3월 11일</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h4>{ title[2] }</h4>
+        <p>3월 12일</p>
         <hr/>
       </div>
     </div>
